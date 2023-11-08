73c77ad35ca8617e936108a6495f9c38b0c8025d
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-11-08 21:48:46 +0100

    completed the challenge set

---
+++ lib/basic_interface.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class BasicInterface extends StatelessWidget {
+  const BasicInterface({super.key});
+
+  @override
+  Widget build(context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 150,
+        ),
+        Image.asset(
+          'assets/images/quiz-logo.png',
+        ),
+        const SizedBox(
+          height: 60,
+        ),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 60,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            //implement event later on
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.deepPurple,
+            foregroundColor: Colors.white,
+          ),
+          child: const Text('Start Quiz'),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/gradient_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/basic_interface.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colorA, this.colorB, {super.key});
+
+  final Color colorA;
+  final Color colorB;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [colorA, colorB],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const BasicInterface(),
+    );
+  }
+}
