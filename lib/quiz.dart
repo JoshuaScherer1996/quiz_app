aef5ea435178db55f6fc197f6b72e2e443f9b2a7
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2023-11-12 20:44:45 +0100

    created new files for more widgets and better execution. Also prepared the new widget to render screens conditionally

---
+++ lib/quiz.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    /* setState reexecutes the build method of the state class to adjust the 
+    screen for changes */
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
