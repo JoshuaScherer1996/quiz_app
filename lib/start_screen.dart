ee4d6e9b06a7c858ff02288c571fe0b17464560e
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-03 00:51:11 +0100

    learned about third party packages and the pub.dev site. Added google fonts and played around with font styles

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -25,10 +26,10 @@ class StartScreen extends StatelessWidget {
           const SizedBox(
             height: 80,
           ),
-          const Text(
+          Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(
-              color: Color.fromARGB(225, 237, 223, 252),
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(225, 237, 223, 252),
               fontSize: 24,
             ),
           ),
