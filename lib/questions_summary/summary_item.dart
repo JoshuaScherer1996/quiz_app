e63776a16f84a7863b2b974e321f630dca19ef68
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-01-03 22:58:09 +0100

    started setting up the ui for the ui challenge in the course. Created the identifier widget for the numbers in the summary screen and started to set up the summary item widget

---
+++ lib/questions_summary/summary_item.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/questions_summary/questions_identifier.dart';
+
+class SummaryItem extends StatelessWidget {}
